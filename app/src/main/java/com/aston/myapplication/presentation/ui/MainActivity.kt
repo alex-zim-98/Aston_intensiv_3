2d0736a1b8e2af7cf24d46f68538bedcc29bbb90
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-12-21 15:14:54 +0000

    use in activity

--- app/src/main/java/com/aston/myapplication/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/aston/myapplication/presentation/ui/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.aston.myapplication.databinding.ActivityMainBinding
+import com.aston.myapplication.domain.entity.Contact
+import com.aston.myapplication.presentation.ui.adapters.CompositeDelegateAdapter
+import com.aston.myapplication.presentation.ui.adapters.ContactDelegateAdapter
 import com.aston.myapplication.presentation.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -20,19 +23,31 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this)[MainViewModel::class.java]
     }
 
-    private lateinit var contactAdapter: ContactListAdapter
+    private val selectedMap = mutableMapOf<Int, Contact>()
+    private var modeDelete = false
+
+    private val contactAdapter by lazy {
+        CompositeDelegateAdapter(
+            listOf(
+                ContactDelegateAdapter(
+                    clickOnItemListener = { contact ->
+                        startActivity(NewContactActivity.newIntentEdit(this, contact))
+                    },
+                    selectedMap = selectedMap,
+                    modeDelete = modeDelete
+                )
+            )
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
-        contactAdapter = ContactListAdapter()
         viewBinding.rvContacts.adapter = contactAdapter
 
         observers()
-
         clickListeners()
-
         setupItemTouchHelper()
     }
 
@@ -45,21 +60,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clickListeners() {
-        contactAdapter.clickOnItemListener = {
-            startActivity(NewContactActivity.newIntentEdit(this, it))
-        }
-
         viewBinding.fabAdd.setOnClickListener {
             startActivity(NewContactActivity.newIntentAdd(this))
         }
 
         viewBinding.btnDelete.setOnClickListener {
-            viewModel.removeListContact(contactAdapter.selectedMap.values.toList())
+            viewModel.removeListContact(selectedMap.values.toList())
         }
 
         viewBinding.fabDelete.setOnClickListener { cancel() }
 
         viewBinding.btnCancel.setOnClickListener { cancel() }
+    }
+
+    private fun cancel() {
+        isClick = !isClick
+        animationVisibility(viewBinding.clDeleteMode, getVisibilityStatus(isClick))
+        animationVisibility(viewBinding.fabAdd, getVisibilityStatus(!isClick))
+        modeDelete = isClick
+        selectedMap.clear()
+        contactAdapter.notifyDataSetChanged()
     }
 
     private fun animationVisibility(view: View, statusVisibility: Int) {
@@ -74,13 +94,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getAlpha(statusVisibility: Int) = if (statusVisibility == View.VISIBLE) 0f else 1f
-
-    private fun cancel() {
-        isClick = !isClick
-        animationVisibility(viewBinding.clDeleteMode, getVisibilityStatus(isClick))
-        animationVisibility(viewBinding.fabAdd, getVisibilityStatus(!isClick))
-        contactAdapter.modeDelete = isClick
-    }
 
     private fun getVisibilityStatus(isClick: Boolean) =
         if (isClick) View.VISIBLE else View.INVISIBLE
@@ -100,13 +113,15 @@ class MainActivity : AppCompatActivity() {
                     return false
                 }
 
-                contactAdapter.moveItem(fromPosition, toPosition)
+                val updatedList = contactAdapter.items.toMutableList()
+                val movedItem = updatedList.removeAt(fromPosition)
+                updatedList.add(toPosition, movedItem)
+
+                contactAdapter.submitList(updatedList)
                 return true
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) = Unit
-
-            override fun getMoveThreshold(viewHolder: RecyclerView.ViewHolder) = 0.1f
         }
         ItemTouchHelper(callback).attachToRecyclerView(viewBinding.rvContacts)
     }
