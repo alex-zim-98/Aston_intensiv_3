efb1eea1c9a10cf55f04cec588340915282ca8eb
alex-zim-98 <alexandr.zimarev.64@gmail.com>
2024-12-22 04:16:10 +0000

    contact adapter

--- app/src/main/java/com/aston/myapplication/presentation/ui/adapters/ContactDelegateAdapter.kt
+++ app/src/main/java/com/aston/myapplication/presentation/ui/adapters/ContactDelegateAdapter.kt
@@ -3,6 +3,7 @@ package com.aston.myapplication.presentation.ui.adapters
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import com.aston.myapplication.R
 import com.aston.myapplication.databinding.ItemContactBinding
 import com.aston.myapplication.domain.entity.Contact
@@ -11,8 +12,13 @@ import com.aston.myapplication.presentation.ui.ContactListViewHolder
 class ContactDelegateAdapter(
     private val clickOnItemListener: ((item: Contact) -> Unit)?,
     private val selectedMap: MutableMap<Int, Contact>,
-    private val modeDelete: Boolean
+    var modeDelete: Boolean
 ) : DelegateAdapter<Contact, ContactListViewHolder> {
+    private var compositeAdapter: CompositeDelegateAdapter<Contact, RecyclerView.ViewHolder>? = null
+
+    fun setCompositeAdapter(adapter: CompositeDelegateAdapter<Contact, RecyclerView.ViewHolder>) {
+        compositeAdapter = adapter
+    }
 
     override fun isForViewType(item: Contact, position: Int): Boolean = true
 
@@ -33,18 +39,41 @@ class ContactDelegateAdapter(
         val color = if (selectedMap.contains(position)) R.color.gray else R.color.white
         val drawable = ContextCompat.getDrawable(holder.itemView.context, color)
         holder.itemView.background = drawable
+        changeBackgroundView(holder, getColorBackground(holder.adapterPosition))
 
         holder.itemView.setOnClickListener {
-            if (modeDelete) {
-                if (selectedMap.contains(position)) {
-                    selectedMap.remove(position)
-                } else {
-                    selectedMap[position] = item
-                }
-            } else {
-                clickOnItemListener?.invoke(item)
-            }
+            rightMode(holder.adapterPosition, item)
+            compositeAdapter?.notifyItemChangedSafe(holder.adapterPosition)
+        }
+
+    }
+    private fun rightMode(position: Int, contact: Contact) {
+        if (modeDelete) {
+            launchDeleteMode(position, contact)
+        } else {
+            clickOnItemListener?.invoke(contact)
         }
     }
+
+    private fun launchDeleteMode(position: Int, contact: Contact) {
+        if (selectedMap.contains(position)) {
+            selectedMap.remove(position)
+        } else {
+            selectedMap[position] = contact
+        }
+    }
+
+    private fun getColorBackground(position: Int): Int {
+        return if (selectedMap.contains(position)) R.color.gray else R.color.white
+    }
+
+    private fun changeBackgroundView(
+        holder: ContactListViewHolder,
+        color: Int
+    ) {
+        val drawable = ContextCompat.getDrawable(holder.itemView.context, color)
+        holder.itemView.background = drawable
+    }
+
 }
 
